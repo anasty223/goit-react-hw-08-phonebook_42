0e22c43a6a78a857b16567793bfca9398aaab74b
anasty223 <anasty22@gmail.com>
2022-06-22 14:36:53 +0300

    add routes

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,60 @@
+import { NavLink, useLocation } from "react-router-dom";
+import style from "./Navigation.module.css";
+
+const Navigation = () => {
+  const location = useLocation();
+  return (
+    <>
+      {!location.pathname.includes("/details") && (
+        <nav>
+          <ul className={style.list}>
+            <li className={style.listItem}>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) =>
+                  isActive ? style.activeStyle : style.navLink
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className={style.listItem}>
+              <NavLink
+                to={"/phonebook"}
+                className={({ isActive }) =>
+                  isActive ? style.activeStyle : style.navLink
+                }
+              >
+                Phonebook
+              </NavLink>
+            </li>
+            <li className={style.listItem}>
+              <NavLink
+                to={"/register"}
+                className={({ isActive }) =>
+                  isActive ? style.activeStyle : style.navLink
+                }
+              >
+                Registration
+              </NavLink>
+            </li>
+
+            <li className={style.listItem}>
+              <NavLink
+                to={"/login"}
+                className={({ isActive }) =>
+                  isActive ? style.activeStyle : style.navLink
+                }
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
+  );
+};
+
+export default Navigation;
