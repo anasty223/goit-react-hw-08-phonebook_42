c98725e34c289baf7055c057af02f563d4b010d1
anasty223 <anasty22@gmail.com>
2022-06-22 19:04:08 +0300

    add components

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,3 +1,20 @@
+import Container from "../Container/Container";
+import Form from "../Form/Form";
+import Filter from "../Filter/Filter";
+import ContactsList from "../ContactsList/ContactsList";
+import { ToastContainer } from "react-toastify";
+
 export default function Contacts() {
-  return <div>Contacts</div>;
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+
+      <Form />
+
+      <Filter />
+      <h2>Contacts</h2>
+      <ContactsList />
+      <ToastContainer />
+    </Container>
+  );
 }
