334669e580c78bf23e5770220acde7183bb2d4b8
anasty223 <anasty22@gmail.com>
2022-06-27 19:38:52 +0300

    add privat,public pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { useEffect, Suspense, lazy } from "react";
 import { fetchCurrentUser } from "../src/redux/auth/auth-operation";
 import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 const Home = lazy(() => import("./components/Home/Home"));
 const Layout = lazy(() => import("./components/Layout/Layout"));
@@ -17,23 +18,49 @@ function App() {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
   return (
-    <Suspense fallback={<p>Загружаем...</p>}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/register" element={<RegisterView />} />
-          <Route path="/login" element={<LoginView />} />
+    <>
+      <Layout />
+      <Suspense fallback={<p>Загружаем...</p>}>
+        <Routes>
+          {/* <Route path="/" element={<Layout />}> */}
+          {/* <Route index element={<Home />} /> */}
+          {/* <Route path="/register" element={<RegisterView />} /> */}
+          {/* <Route path="/login" element={<LoginView />} /> */}
 
-          {/* <Route path="phonebook" element={<Contacts />} /> */}
+          <Route path="/" element={<PublicRoute component={<Home />} />} />
+
           <Route
-            path="phonebook"
+            path="/register"
+            element={
+              <PublicRoute
+                component={<RegisterView />}
+                navigateTo="/phonebook"
+                restricted
+              />
+            }
+          />
+
+          <Route
+            path="/login"
+            element={
+              <PublicRoute
+                component={<LoginView />}
+                navigateTo="/phonebook"
+                restricted
+              />
+            }
+          />
+
+          <Route
+            path="/phonebook"
             element={
               <PrivateRoute component={<Contacts />} navigateTo="/login" />
             }
           />
-        </Route>
-      </Routes>
-    </Suspense>
+          {/* </Route> */}
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
