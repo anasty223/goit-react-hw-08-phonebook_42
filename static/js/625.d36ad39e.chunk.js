8e5b878d3d6a97e25812a95afa5ed20d27a7d891
anasty223 <anasty22@gmail.com>
2022-07-01 19:44:54 +0300

    Updates

---
+++ static/js/625.d36ad39e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook_42=self.webpackChunkgoit_react_hw_08_phonebook_42||[]).push([[625],{6625:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(184);function r(){return(0,t.jsx)("div",{children:"Home"})}}}]);
+//# sourceMappingURL=625.d36ad39e.chunk.js.map
